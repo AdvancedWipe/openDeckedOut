3f6d4f0aac06dc22550edb7ef5f29eb86fa5fb8a
AdvancedWipe <65723138+AdvancedWipe@users.noreply.github.com>
2023-11-09 20:40:24 +0100

    add test for method isInArena()

--- src/test/java/com/github/advancedwipe/opendeckedout/utils/DungeonUtilsTest.java
+++ src/test/java/com/github/advancedwipe/opendeckedout/utils/DungeonUtilsTest.java
@@ -1,6 +1,7 @@
 package com.github.advancedwipe.opendeckedout.utils;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 
@@ -10,6 +11,35 @@ import org.bukkit.World;
 import org.junit.jupiter.api.Test;
 
 public class DungeonUtilsTest {
+
+  @Test
+  void isInArena_ShouldReturnTrue_WhenLocationInsideBoundingBox() {
+
+    World world = mock(World.class);
+    Location pos1 = new Location(world, 0, 0, 0);
+    Location pos2 = new Location(world, 10, 10, 10);
+    Location locationInside = new Location(pos1.getWorld(), 5, 5, 5);
+
+    assertTrue(DungeonUtils.isInArena(locationInside, pos1, pos2));
+  }
+
+  @Test
+  void isInArena_ShouldReturnFalse_WhenLocationOutsideBoundingBox() {
+    Location pos1 = new Location(mock(World.class), 0, 0, 0);
+    Location pos2 = new Location(mock(World.class), 10, 10, 10);
+    Location locationOutside = new Location(pos1.getWorld(), 15, 15, 15);
+
+    assertFalse(DungeonUtils.isInArena(locationOutside, pos1, pos2));
+  }
+
+  @Test
+  void isInArena_ShouldReturnFalse_WhenDifferentWorlds() {
+    Location pos1 = new Location(mock(World.class), 0, 0, 0);
+    Location pos2 = new Location(mock(World.class), 10, 10, 10);
+    Location location = new Location(mock(World.class, "world2"), 5, 5, 5);
+
+    assertFalse(DungeonUtils.isInArena(location, pos1, pos2));
+  }
 
   @Test
   void getBlocksInBoundingBox_ShouldReturnCorrectLocations() {
