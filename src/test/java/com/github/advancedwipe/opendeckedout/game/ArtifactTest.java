25230a332b9ca7fc909e57052579e978bf668cce
AdvancedWipe <65723138+AdvancedWipe@users.noreply.github.com>
2023-11-10 20:25:50 +0100

    add tests for Artifact

---
+++ src/test/java/com/github/advancedwipe/opendeckedout/game/ArtifactTest.java
@@ -0,0 +1,28 @@
+package com.github.advancedwipe.opendeckedout.game;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.github.advancedwipe.opendeckedout.game.artifact.Artifact;
+import org.bukkit.Location;
+import org.junit.jupiter.api.Test;
+
+public class ArtifactTest {
+
+  private final double x = 10.5;
+  private final double y = 20.3;
+  private final double z = 30.7;
+  private final float yaw = 45.0f;
+  private final float pitch = 30.0f;
+  private final Location location = new Location(null, x, y, z, yaw, pitch);
+  private final String locationString = x + ";" + y + ";" + z + ";" + yaw + ";" + pitch;
+
+  @Test
+  void toString_ShouldReturnExpectedString_WithValidInput() {
+    Artifact artifact = new Artifact(location, 2, "Medium");
+
+    String result = artifact.toString();
+
+    assertEquals(locationString + ";2;Medium", result);
+  }
+
+}
