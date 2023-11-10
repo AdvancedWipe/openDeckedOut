426a18e6ce6e59702c227045ad6d8de540c1e03a
AdvancedWipe <65723138+AdvancedWipe@users.noreply.github.com>
2023-11-10 20:34:45 +0100

    add tests for PlayerSensor

---
+++ src/test/java/com/github/advancedwipe/opendeckedout/game/PlayerSensorTest.java
@@ -0,0 +1,52 @@
+package com.github.advancedwipe.opendeckedout.game;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.junit.jupiter.api.Test;
+
+public class PlayerSensorTest {
+
+  private final double x = 10.5;
+  private final double y = 20.3;
+  private final double z = 30.7;
+  private final float yaw = 45.0f;
+  private final float pitch = 30.0f;
+  private final Location location = new Location(null, x, y, z, yaw, pitch);
+  private final Player player = mock(Player.class);
+
+  @Test
+  void constructor_ShouldInitializeLocationAndCooldownState() {
+
+    PlayerSensor playerSensor = new PlayerSensor(location);
+
+    assertEquals(location, playerSensor.getLocation());
+    assertFalse(playerSensor.hasCooldown());
+  }
+
+  @Test
+  void activated_ShouldSetCooldownForPlayer() {
+    PlayerSensor playerSensor = new PlayerSensor(location);
+
+    playerSensor.activated(player);
+
+    assertTrue(playerSensor.hasCooldown());
+  }
+
+  @Test
+  void decreaseCooldown_ShouldDecreaseCooldownForActivatedPlayer() {
+    PlayerSensor playerSensor = new PlayerSensor(location);
+    playerSensor.activated(player);
+
+    playerSensor.decreaseCooldown();
+    playerSensor.decreaseCooldown();
+    playerSensor.decreaseCooldown();
+    playerSensor.decreaseCooldown();
+
+    assertFalse(playerSensor.hasCooldown());
+  }
+}
